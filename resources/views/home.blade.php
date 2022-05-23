4a1923210d2264334546ed765c03b7cff2c4607e
grotiuss <grotius.hasiholan@gmail.com>
2022-05-23 15:36:57 +0700

    Using blade templating engine

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/home.css">
-    <title><?= $tab_label?></title>
+    {{-- <title><?= $tab_label?></title> --}}
+    <title>{{ $tab_label }}</title>
 </head>
 <body>
-    <h1><?= $main_text?></h1>
+    <!-- <h1><?= $main_text?></h1> -->
+    <h1>{{ $main_text }}</h1>
     <script src="js/home.js"></script>
 </body>
 </html>
