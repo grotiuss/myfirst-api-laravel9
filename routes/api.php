4138575051a5e6e7f8147bc617349efa7f21b02c
grotius.hasiholan <grotius.hasiholan@gmail.com>
2022-06-03 15:12:30 +0700

    Adding double method route

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::match(['get', 'post'], '/test', function(Request $request) { 
+    //we can use more than one different method for the same route without create more
+    $data = [
+        'status' => 200,
+        'messagge' => "Hello motto!!!"
+    ];
+    return response()->json($data, $data['status']);
+});
+
 Route::get('/test-get', function(Request $request) {
     $data = [
         'status' => 200,
